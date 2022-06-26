862edcfb449bae5a77d5ff7cc0bc7180b4946b4d
Taran Tuteja <taran.tuteja@yahoo.com>
2022-06-26 15:51:20 +0530

    Added solution - LeetHub

--- Stock buy and sell - GFG/stock-buy-and-sell.cpp
+++ Stock buy and sell - GFG/stock-buy-and-sell.cpp
@@ -1,50 +1,77 @@
 // { Driver Code Starts
-// Program to find best buying and selling days
-#include <bits/stdc++.h>
+//Initial template for C++
 
+#include <bits/stdc++.h>
 using namespace std;
 
-// This function finds the buy sell schedule for maximum profit
-void stockBuySell(int *, int);
 
-// Driver program to test above functions
-int main() {
-    int T;
-    cin >> T;
+ // } Driver Code Ends
+//User function template for C++
 
-    while (T--) {
-        int n, i;
-        cin >> n;
-        int price[n];
-        for (i = 0; i < n; i++) cin >> price[i];
-        // function call
-        stockBuySell(price, n);
-    }
-    return 0;
-}
-// } Driver Code Ends
-
-
-// User function template for C++
-
-// This function finds the buy sell schedule for maximum profit
-void stockBuySell(int price[], int n) {
-    vector<pair<int,int> >ans;
+class Solution{
+public:
+    //Function to find the days of buying and selling stock for max profit.
+    vector<vector<int> > stockBuySell(vector<int> A, int n){
+        // code here
+        vector<pair<int,int> >ans;
     int i=0;
     while(i<n){
-        for(i;i<n-1 && price[i+1]<=price[i];i++);
+        for(i;i<n-1 && A[i+1]<=A[i];i++);
         if(i==n-1)
             break;
         int buy = i;
-        for(i;i<n-1 && price[i+1]>=price[i];i++);
+        for(i;i<n-1 && A[i+1]>=A[i];i++);
         int sell = i;
         ans.push_back({buy,sell});
     }
-    if(ans.empty()){
-        printf("No Profit\n");
-        return;
+    vector<vector<int>> res ; 
+    for(auto x : ans ){
+        vector<int> v ;
+        v.push_back(x.first) ; 
+        v.push_back(x.second) ; 
+        res.push_back(v); 
     }
-    for(auto it: ans)
-        printf("(%d %d) ",it.first,it.second);
-    printf("\n");
+    return res ; 
+    }
+};
+
+// { Driver Code Starts.
+
+int check(vector<vector<int>> ans, vector<int> A, int p)
+{
+    int c = 0;
+    for(int i=0; i<ans.size(); i++)
+        c += A[ans[i][1]]-A[ans[i][0]];
+    return (c==p) ? 1 : 0;
 }
+
+int main()
+{   
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        vector<int> A(n);
+        for (int i=0; i<n; ++i){
+            cin>>A[i];
+        }
+        Solution ob;
+        vector<vector<int> > ans = ob.stockBuySell(A, n);
+        int p = 0;
+        for(int i=0; i<n-1; i++)
+        {
+            int x = A[i+1]-A[i];
+            if(x>0)
+                p += x;
+        }
+        if(ans.size()==0)
+            cout<<"No Profit";
+        else{
+            cout<<check(ans,A,p);
+        }cout<<endl;
+    }
+    return 0;
+}
+
+  // } Driver Code Ends
