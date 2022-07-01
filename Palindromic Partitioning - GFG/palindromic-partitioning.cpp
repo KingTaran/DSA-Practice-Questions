823b331dd2a0e7118d4b7a696acace00b3e7dc81
Taran Tuteja <taran.tuteja@yahoo.com>
2022-07-02 00:37:28 +0530

    Added solution - LeetHub

--- Palindromic Partitioning - GFG/palindromic-partitioning.cpp
+++ Palindromic Partitioning - GFG/palindromic-partitioning.cpp
@@ -6,6 +6,10 @@ using namespace std;
 
  // } Driver Code Ends
 // User function Template for C++
+
+
+/* This Question is  Based on Dynamic Programing Matrix Chain Multiplication  */
+
 int t[1000][1000]; 
 
 class Solution{
@@ -19,21 +23,29 @@ bool isPalindrome(string &s , int i , int j ){
      }
      return 1 ; 
 }
+// This Solve Function is Similar to Matrix Chain Multiplicaton  
 int Solve(string &s , int i , int j ){
+    // Base Case
     if(i >= j ){
         return 0 ; 
     }
+    // If the string is already a Palindome,  then we Will return 0;  
     if(isPalindrome(s , i , j ) == 1){
         return 0 ; 
     }
+    
+    // This is The Dp array
     if(t[i][j] != -1){
+        // If  the element is Already Present in the Matrix, we return the  
         return t[i][j]; 
     }
+    // The main solution patiotoning Part
     int ans =INT_MAX; 
     for(int k = i ; k < j ; k++){
         int tempans = Solve(s , i , k ) + Solve(s , k + 1 , j ) + 1; 
         ans = min(ans , tempans); 
     }
+    // Storing the Solution in DP matrix 
    t[i][j] = ans ;
    return ans ; 
     
@@ -41,7 +53,8 @@ int Solve(string &s , int i , int j ){
 }
     int palindromicPartition(string str)
     {
-        memset(t , -1 , sizeof(t)); 
+        // Initalizing  all the DP matrix element with -1
+        memset(t , -1 , sizeof(t));
         return Solve(str, 0 , str.length()-1 ) ; 
     }
 };
