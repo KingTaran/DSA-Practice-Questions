ed7bd80f975d649c4cdd7104cf2fa2ad18df404c
Taran Tuteja <taran.tuteja@yahoo.com>
2022-07-13 12:57:28 +0530

    Added solution - LeetHub

---
+++ Maximum Sum Problem - GFG/maximum-sum-problem.cpp
@@ -0,0 +1,33 @@
+// { Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+class Solution
+{
+    public:
+        int maxSum(int n)
+        {
+            int dp[n + 1]; 
+            fill(dp , dp + n  +1 , INT_MIN );
+            for(int i = 0 ; i <n+ 1 ; i ++){
+                dp[i] = max(i , dp[i/ 3]+ dp[i/2]+ dp[i/4] ); 
+            }  
+            return dp[n]; 
+        }
+};
+
+// { Driver Code Starts.
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        Solution ob;
+        cout<<ob.maxSum(n)<<"\n";
+    }
+    return 0;
+}  // } Driver Code Ends
