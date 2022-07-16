fde70319b195816e9a46696393974c7ce5e296d2
Taran Tuteja <taran.tuteja@yahoo.com>
2022-07-16 15:56:30 +0530

    Time: 7 ms (98.32%), Space: 11.4 MB (96.64%) - LeetHub

--- 740-delete-and-earn/740-delete-and-earn.cpp
+++ 740-delete-and-earn/740-delete-and-earn.cpp
@@ -3,13 +3,14 @@ public:
     int deleteAndEarn(vector<int>& nums) {
      int inc =  0 ; 
      int ecl = 0 ; 
+        int N = *max_element(nums.begin() , nums.end()); 
         int n = nums.size() ; 
-        int count[10001] ; 
-    fill(count , count + 10001 ,  0 ) ; 
+        int count[N+1] ; 
+    fill(count , count + N+1 ,  0 ) ; 
         for(int i = 0 ; i<n ; i++){
             count[nums[i]] ++  ; 
         }
-        for(int i = 0 ; i<= 10000 ; i++){
+        for(int i = 0 ; i<= N ; i++){
             int ni = ecl  + count[i] * i ; 
             int ne = max(ecl , inc) ; 
             inc= ni ; 
