cbdb92a8b37683fea4417924f3c583052ece0152
Taran Tuteja <taran.tuteja@yahoo.com>
2022-08-16 11:27:54 +0530

    Time: 8 ms (22.95%), Space: 7.2 MB (21.18%) - LeetHub

--- 376-wiggle-subsequence/376-wiggle-subsequence.cpp
+++ 376-wiggle-subsequence/376-wiggle-subsequence.cpp
@@ -1,29 +1,15 @@
 class Solution {
 public:
     int wiggleMaxLength(vector<int>& nums) {
-        if(nums.size() == 0 ){
-            return 0 ; 
-        }
-        if(nums.size() == 1){
-            return 1; 
-        }
-        int prev_diff = nums[0] - nums[1];
-        int n = nums.size(); 
-        int diff = 0 ; 
-        int count = prev_diff != 0 ? 2 : 1; 
-        for(int i = 2 ; i< n ; i++){
-            diff= nums[i-1] -nums[i] ; 
-            if(prev_diff <= 0 && diff > 0) {
-                count ++; 
-                prev_diff= diff ; 
-            } 
-            else if(prev_diff >= 0 && diff < 0 ){
-                count ++; 
-                prev_diff = diff; 
-            } 
-        }
-        return count ; 
-        
-        
-     }
+        int peak = 1 , valley = 1;
+         for(int i = 1; i < nums.size() ; i++){
+             if(nums[i] > nums[i-1]){
+                 peak = valley +1 ; 
+             }
+             else if(nums[i-1] > nums[i]){
+                 valley  = peak + 1; 
+             }
+         }
+        return max(peak , valley ); 
+    }
 };
