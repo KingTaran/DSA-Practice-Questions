a817dcfbfa08a69abcae319bee6fb5a7b00a3a5b
Taran Tuteja <taran.tuteja@yahoo.com>
2022-08-25 23:42:10 +0530

    Time: 74 ms (50.93%), Space: 21.5 MB (63.38%) - LeetHub

--- 639-decode-ways-ii/639-decode-ways-ii.cpp
+++ 639-decode-ways-ii/639-decode-ways-ii.cpp
@@ -1,57 +1,48 @@
-int mod = 1e9 + 7;
 class Solution {
 public:
-    long long int func(string &s , int index ,vector<long long int> &dp  ){
-        if(index >= s.length()){
-            return 1 ; 
-        }
-        if(dp[index] != -1){
-            return dp[index]; 
-        }
-        
-        if(s[index] =='0'){
-            return dp[index] =  0 ; 
-        }
-        long long int ans = func(s , index + 1 , dp) ; 
-        if(s[index] == '*'){
-            ans *= 9 ; 
-            
-            if(index + 1 < s.length() && s[index + 1] >= '0' && s[index+1] <= '9'){
-                ans += func(s , index + 2 , dp ); 
-            }
-            
-            if(index + 1 < s.length()  && s[index + 1] == '*'){
-                ans += 9 *func(s, index + 2, dp ); 
-            }
-            if(index + 1 < s.length() && s[index + 1] >= '0' && s[index + 1] <= '6' )
-            {
-                ans += func(s, index + 2 ,dp ); 
-            }
-            if(index + 1 < s.length() && s[index + 1] =='*' ){
-                ans = ans + (6*func(s, index + 2 ,dp)); 
-            }
-            
-        }
-        else if(s[index] =='1' && index+1 < s.length()){
-            if(s[index + 1] >= '0' && s[index + 1] <= '9' ){
-                ans += func(s, index + 2 ,dp ); 
-            }
-            else if(s[index + 1]  == '*'){
-                ans += (9*func(s , index + 2 ,dp)); 
-            }
-        } 
-        else if(s[index] == '2' && index + 1 < s.length()){
-            if(s[index + 1] >= '0' && s[index + 1] <= '6'){
-                ans += func(s , index + 2 ,dp); 
-            }
-            else if(s[index + 1] == '*'){
-                ans += (6*func(s , index + 2 ,dp)); 
-            }
-        }
-        return dp[index] = ans%mod ; 
-    }
+
+	// TIME COMPLEXITY:- O(N)
+	// SPACE COMPLEXITY:- O(N)
+    
+    #define ll long long
+    #define MOD 1000000007
+    // dp[i] = number of ways to decode the substring(0,i-1)
     int numDecodings(string s) {
-          vector<long long int> dp (s.length() + 1 , -1); 
-        return func(s , 0 ,dp  ); 
+        vector<ll> dp(s.length()+1,0);
+        if(s[0]=='0') // no ways to decode
+            return 0;
+        dp[0] = 1; // base case
+        dp[1] = (s[0]=='*')?9:1;
+        for(ll i=2;i<=s.length();i++){
+            char curr = s[i-1],prev = s[i-2];
+            // introducing first barrier like (12)(3)
+            if(curr=='*')
+                dp[i] = (dp[i]+(9*dp[i-1])%MOD)%MOD;
+            else if(curr!='0')
+                dp[i] = (dp[i]+dp[i-1])%MOD;
+            
+            // introducing second barrier like (1)(23)
+            if(prev=='*'){
+                if(curr=='*')
+                    dp[i] = (dp[i]+(15*dp[i-2])%MOD)%MOD;
+                else if(curr<='6')
+                    dp[i] = (dp[i]+(2*dp[i-2])%MOD)%MOD;
+                else
+                    dp[i] = (dp[i]+dp[i-2])%MOD;
+            }
+            else if(prev=='1'){
+                if(curr=='*')
+                    dp[i] = (dp[i]+(9*dp[i-2])%MOD)%MOD;
+                else
+                    dp[i] = (dp[i]+dp[i-2])%MOD;
+            }
+            else if(prev=='2'){
+                if(curr=='*')
+                    dp[i] = (dp[i]+(6*dp[i-2])%MOD)%MOD;
+                else if(curr<='6')
+                    dp[i] = (dp[i]+dp[i-2])%MOD;
+            }
+        }
+        return dp.back();
     }
 };
