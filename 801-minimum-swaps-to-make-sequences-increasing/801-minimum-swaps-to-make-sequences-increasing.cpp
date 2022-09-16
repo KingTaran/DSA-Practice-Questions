948328baf1915ce9279ee791f70c547585dca633
Taran Tuteja <taran.tuteja@yahoo.com>
2022-09-16 11:13:09 +0530

    Time: 317 ms (71.43%), Space: 131.3 MB (24.87%) - LeetHub

--- 801-minimum-swaps-to-make-sequences-increasing/801-minimum-swaps-to-make-sequences-increasing.cpp
+++ 801-minimum-swaps-to-make-sequences-increasing/801-minimum-swaps-to-make-sequences-increasing.cpp
@@ -1,24 +1,25 @@
 class Solution {
 public:
-    int func(vector<int > &a , vector<int> &b , int prev_a , int prev_b  , int i , bool swaped,  vector<vector<int>>&dp){
-        if( i == a.size()){
+    int func(vector<int> &a ,vector<int> & b , int prev_a , int prev_b , int i , bool swap , vector<vector<int>> &dp){
+        if(i == a.size()){
             return 0 ; 
         }
-        if(dp[i][swaped] != -1){
-           return dp[i][swaped]; 
+        if(dp[i][swap] != -1){
+            return dp[i][swap] ; 
         }
-        int res = INT_MAX ; 
-        if(a[i] > prev_a && b[i]  > prev_b){
-            res =  func(a,  b , a[i] , b[i] , i+1, 0 , dp); 
+        int no_swap = INT_MAX; 
+        if(a[i] > prev_a && b[i] > prev_b){
+            no_swap = func(a, b , a[i] , b[i] , i + 1 ,  0 , dp ); 
         }
-        int res2 = INT_MAX; 
-        if(a[i] > prev_b && b[i] > prev_a ){
-            res2 = 1+ func(a,  b , b[i] , a[i] , i + 1, 1 , dp)  ; 
+        int swapp = INT_MAX ; 
+        if(a[i] > prev_b && b[i] > prev_a){
+            swapp = min(swapp , 1 + func(a,  b , b[i] , a[i] , i + 1 , 1 , dp )); 
         }
-        return dp[i][swaped] = min(res , res2 ) ; 
+        return dp[i][swap]  = min(swapp , no_swap); 
+        
     }
     int minSwap(vector<int>& nums1, vector<int>& nums2) {
-        vector<vector<int>>dp (nums1.size() + 1 , vector<int> (2 , -1)); 
+        vector<vector<int>> dp(nums1.size() + 1 , vector<int> (2 , -1)); 
         return func(nums1 , nums2 , -1 , -1 , 0 , 0 , dp ); 
     }
 };
