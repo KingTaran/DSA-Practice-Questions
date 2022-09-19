3431cf47bde1ab7962affbb0c102a183fc541440
Taran Tuteja <taran.tuteja@yahoo.com>
2022-09-19 22:11:57 +0530

    Time: 745 ms (14.83%), Space: 85.3 MB (13.06%) - LeetHub

--- 1690-stone-game-vii/1690-stone-game-vii.cpp
+++ 1690-stone-game-vii/1690-stone-game-vii.cpp
@@ -1,45 +1,26 @@
-
-// IMPORTANT QUESTION and explaination 
-
-
-// sum - v[i] Represents the amount of profit that player1 can make 
-        // (-) sign is for the diffrence 
-        // func(v , i + 1 , j  ,sum - v[i]) represents max profit saamne wala can make offically 
-        // similarly 
-        // if player 1 chooses last element
-        // func(v , i  , j-1 , sum - v[j]) represents max profit saamne wala can make if the player one chooses last element 
-
-
 class Solution {
 public:
     int stoneGameVII(vector<int>& stones) {
+        dp.resize(stones.size() + 1 , vector<int>(stones.size() + 1 , -1));  
         int sum = 0 ; 
-        sum = accumulate(stones.begin() , stones.end() , sum ); 
-        
-        dp.resize(stones.size() + 1 , vector<int> ( stones.size() + 1 , -1)); 
-        int x = func(stones , 0 , stones.size() -1 , sum ); 
-        
-        // int y = min( func(stones , 1 , stones.size() -1) ,  func(stones , 0 , stones.size() -2)); 
-        return x ; 
+        sum  = accumulate(stones.begin() , stones.end() , sum ); 
+        return func(stones , 0 , stones.size() -1 , sum); 
     }
-private : 
+private: 
     vector<vector<int>> dp ; 
-    int func(vector<int> & v ,int  i  , int j , int sum  ){
-        if(i == j ){
-            return 0 ; 
+    int func(vector<int> &v  , int i , int j , int sum  ){
+        if( i== j ){
+            return 0   ; 
         }
-        if(i > j ){
+        if(i > j ) {
             return 0 ; 
         }
         if(dp[i][j] != -1){
             return dp[i][j]; 
         }
-       
-        
-        int choice1 = sum- v[i] - func(v , i + 1 , j  ,sum - v[i]) ; 
-        int choice2 = sum- v[j] - func(v , i  , j-1 , sum - v[j]);  
+        int choice1 = sum - v[i] - func(v , i + 1 , j , sum - v[i] ); 
+        int choice2= sum -v[j] - func(v, i , j -1 , sum-v[j]); 
         return dp[i][j] = max(choice1 , choice2); 
-        
         
         
         
