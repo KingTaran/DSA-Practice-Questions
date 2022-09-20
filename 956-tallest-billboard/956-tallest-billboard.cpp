33b63485981fe66ed359b84bf8fd1b9813f4c3f7
Taran Tuteja <taran.tuteja@yahoo.com>
2022-09-20 21:27:20 +0530

    Time: 109 ms (64.95%), Space: 60 MB (32.99%) - LeetHub

--- 956-tallest-billboard/956-tallest-billboard.cpp
+++ 956-tallest-billboard/956-tallest-billboard.cpp
@@ -1,26 +1,28 @@
 class Solution {
 public:
-    int dp[21][5005]; 
-    int func(vector<int> &v , int left , int right , int i  ){
-        if(i ==  v.size() ){
-            if(left == right ){
-                return 0 ; 
-            }
-            return -1000000; 
-        }
     
-    if(dp[i][abs(left - right)] != -1)
-    {
-        return dp[i][abs(left - right)]; 
-    }
-    return dp[i][abs(left - right)] = max(v[i] + func(v ,  left + v[i] ,  right ,  i+1  ) , max(v[i] +func(v , left ,  right+ v[i] ,  i+1  ), func(v ,  left ,  right , i+1  )));
-    }
-        
     int tallestBillboard(vector<int>& rods) {
-        memset(dp , -1 , sizeof(dp)); 
-     int sum =  0 ; 
-        for(auto rod : rods ) { 
-            sum += rod; 
+        dp.resize(23 , vector<int> (5005 , -1)); 
+        return func(rods , 0 , 0 , 0  )/2 ; 
     }
-        return func(rods , 0 , 0 , 0)/2; }
+private : 
+    vector<vector<int>> dp ; 
+    int func(vector<int> & v , int left  , int right , int i  ){
+        if(i == v.size()){
+            if(left == right){
+                return 0;
+            }
+            return -10000;
+        }
+       if(dp[i][abs(left - right) ] != -1){
+           return dp[i][abs(left-right)]; 
+       } 
+        
+        
+        return dp[i][abs(left-right)] = max(func(v ,left , right , i + 1 ) , max(v[i] + func(v, left + v[i] , right , i + 1) ,v[i] + func(v, left  , right + v[i] , i + 1)  )); 
+        
+        
+        
+        
+    } 
 };
