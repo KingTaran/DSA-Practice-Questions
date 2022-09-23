7529976639c3d951eeeef2861138142fe73657e9
Taran Tuteja <taran.tuteja@yahoo.com>
2022-09-23 21:48:50 +0530

    Time: 107 ms (5.07%), Space: 20.5 MB (8.79%) - LeetHub

--- 139-word-break/139-word-break.cpp
+++ 139-word-break/139-word-break.cpp
@@ -1,35 +1,36 @@
 class Solution {
 public:
     bool wordBreak(string s, vector<string>& wordDict) {
-        map<string , bool > m ; 
+     map<string , bool > m ;
         for(auto x : wordDict){
             m[x] = 1; 
         }
-        return func(s , m ); 
+        return  func(s , m ) ; 
+        
     }
     
-private :
-    unordered_map<string , bool > dp ; 
-    bool  func(string s  , map<string , bool>  &dict ){
-        if(dict.find(s) != dict.end()){
-            return 1; 
-        }
-        if(dp.find(s)!= dp.end()){
-            return dp[s]; 
-        }
-        
-        for(int i = 0 ; i< s.size() ; i++){
-            string left = s.substr( 0 ,i+1 ); 
-            string right = s.substr(i + 1); 
-            if(dict.find(left) != dict.end()){
-                bool ros = func(right,  dict);
-                if(ros==1 ){
-                    dp[s] = 1; 
-                    return 1; 
-                }
+    
+private:
+    map<string , bool > dp ; 
+    
+int func(string s , map<string , bool > &m ){
+    if(m.find(s) != m.end() ) {
+        return 1; 
+    }
+    if(dp.find(s) != dp.end()){
+        return dp[s]; 
+    }
+    for(int i = 0 ; i< s.size() ; i++){
+        string left = s.substr(0 , i +1); 
+        string right = s.substr(i + 1); 
+        if(m.find(left ) != m.end()){
+            bool res  = func(right , m ) ; 
+            if(res ){
+                return dp[s] = 1; 
             }
         }
-        dp[s] = 0 ; 
-         return 0 ; 
     }
+    return dp[s] = 0 ; 
+    
+}
 };
