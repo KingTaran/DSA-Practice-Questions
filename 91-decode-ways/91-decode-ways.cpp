5ef591eec099c4b9d310fc23bee095908253de6f
Taran Tuteja <taran.tuteja@yahoo.com>
2022-10-03 00:47:42 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (49.07%) - LeetHub

--- 91-decode-ways/91-decode-ways.cpp
+++ 91-decode-ways/91-decode-ways.cpp
@@ -1,34 +1,26 @@
 class Solution {
 public:
-    int mod = 1e9 + 7 ; 
-     int helper(int i , string &s , vector<int> &dp ){
+    int numDecodings(string s) {
+        dp.resize(s.size() + 1 , -1); 
+        return func(s ,0 ) ; 
+    }
+private : 
+    vector<int> dp; 
+    int func(string &s , int i ){
+        if(i >= s.size()){
+            return 1 ; 
+        }
          if(s[i] == '0'){
              return 0 ; 
-         }
-         if(i >= s.size()-1){
-             return 1; 
-         }
-         
-         if(dp[i] != -1){
-             return dp[i] ; 
-         }
-         if(i + 1 < s.size() && (s[i] == '1' || (s[i] == '2' && (s[i + 1] >=48 && s[i+ 1] <= 54 )))) {
-             dp[i] = helper(i + 1 , s , dp ) + helper(i + 2 , s , dp );   
-             return dp[i]  ; 
-         }
-         else{
-             dp[i] = helper(i + 1 , s ,dp );
-             return dp[i]  ; 
-             
-         }
-         
-         
-         
-     }
-    
-    
-    int numDecodings(string s) {
-        vector< int> dp (s.size() + 1 , -1); 
-        return helper(0 , s , dp ); 
+          }
+        if(dp[i] != -1){
+            return dp[i]; 
+        }
+        if(i + 1 < s.size() && (s[i] =='1' || (s[i] == '2' &&(  s[i + 1 ]<= '6' && s[i+1] >= '0' ) )) ){
+            return  dp[i] = func(s , i + 1) + func(s , i + 2); 
+        } 
+        else{
+            return dp[i] = func(s , i + 1); 
+        }
     }
 };
