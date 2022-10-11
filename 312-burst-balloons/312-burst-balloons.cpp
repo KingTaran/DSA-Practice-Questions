c7659d0960f36fe5d8fbe74f62a9e8afbd92f28f
Taran Tuteja <taran.tuteja@yahoo.com>
2022-10-11 11:18:26 +0530

    Time: 724 ms (49.62%), Space: 10.5 MB (19.49%) - LeetHub

--- 312-burst-balloons/312-burst-balloons.cpp
+++ 312-burst-balloons/312-burst-balloons.cpp
@@ -10,17 +10,17 @@ public:
 private : 
     vector<vector<int>>dp ; 
     int func(int i , int j , vector<int> &v ){
-        if(i > j ){
-            return 0 ; 
-        }
-        if(dp[i][j] != -1){
-            return dp[i][j] ; 
-        }
+       if(i > j ){
+           return 0; 
+       } 
+      if(dp[i][j] != -1){
+          return dp[i][j]; 
+      }
         int res = INT_MIN ; 
-        for(int index = i ; index <= j ; index ++ ){
-            int cost = (v[i-1] * v[index ] * v[j + 1] ) + func(i , index - 1 , v ) + func(index + 1 , j , v); 
-            res = max(res  , cost); 
-        }
+       for(int index = i ; index <=j ; index ++ ){
+           int t = (v[i - 1 ] * v[index]* v[j + 1] ) + func(i , index-1 , v ) + func(index +1  , j , v ); 
+           res = max(res , t); 
+       } 
         return dp[i][j] = res ; 
         
         
