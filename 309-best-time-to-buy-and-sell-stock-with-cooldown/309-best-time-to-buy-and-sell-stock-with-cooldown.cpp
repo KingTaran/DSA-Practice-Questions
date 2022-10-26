467e527b6eda5242a5cc0f50213b063caa7c406d
Taran Tuteja <taran.tuteja@yahoo.com>
2022-10-26 15:00:45 +0530

    Time: 16 ms (19.08%), Space: 12 MB (36.19%) - LeetHub

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -1,22 +1,26 @@
 class Solution {
 public:
-    int func(vector<int> v ,int i , int buy , int n , vector<vector<long long>>&dp){
-        if(i >= n ){
+    
+    int maxProfit(vector<int>& prices) {
+        dp.resize(prices.size() + 1 , vector<long long> (2 , -1)); 
+        return func(prices , 0 , 1); 
+    }
+private : 
+    vector<vector<long long >> dp ; 
+    int func(vector<int> & v  ,int  i ,  bool buy ){
+        if(i >= v.size() ){
             return 0 ; 
         }
         if(dp[i][buy] != -1){
             return dp[i][buy]; 
         }
+        
         if(buy){
-            return dp[i][buy] = max( -v[i] + func(v , i + 1 , 0, n , dp) ,func(v , i + 1 , 1, n , dp) ); 
+            return dp[i][buy] =  max(-v[i]  + func(v , i+1  , 0 ) , func(v , i+1 , 1  )); 
         }
         else{
-           return  dp[i][buy] = max( v[i] + func(v , i + 2 , 1, n , dp) ,func(v , i + 1 , 0, n , dp) ); 
+            return dp[i][buy] =  max(v[i] + func(v , i + 2 , 1 ) , func(v , i + 1 , 0)); 
         }
         
-    }
-    int maxProfit(vector<int>& prices) {
-        vector<vector<long long>>dp(prices.size() + 1 , vector<long long>(2 , -1)); 
-        return func(prices , 0 , 1 , prices.size() , dp); 
     }
 };
