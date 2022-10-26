1af157cc8e6aef76ef516a2bb004577ff4204a15
Taran Tuteja <taran.tuteja@yahoo.com>
2022-10-26 13:38:11 +0530

    Time: 448 ms (34.11%), Space: 9.1 MB (69.15%) - LeetHub

--- 279-perfect-squares/279-perfect-squares.cpp
+++ 279-perfect-squares/279-perfect-squares.cpp
@@ -6,13 +6,13 @@ public:
             dp[i] = i ;
         }
         int count =1; 
-        while(count*count <= n ){
-            int sq = count*count; 
-            for(int i = sq ; i<= n ; i++){
-                dp[i ] = min(dp[i] , dp[i-sq] + 1 ); 
-            }
-            count+= 1; 
-        }
+       while(count * count <= n ){
+           int sq = count *count ; 
+           for(int i = sq ; i<=n ; i++){
+               dp[i] = min(dp[i] , dp[i-sq] + 1 ); 
+           }
+           count += 1; 
+       }
         return dp[n ] ; 
     }
 };
