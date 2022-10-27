fcc9a714b905d719f0eb9499ae03867f00586d34
Taran Tuteja <taran.tuteja@yahoo.com>
2022-10-27 12:30:41 +0530

    Time: 108 ms (56.86%), Space: 41.5 MB (21.37%) - LeetHub

--- 1262-greatest-sum-divisible-by-three/1262-greatest-sum-divisible-by-three.cpp
+++ 1262-greatest-sum-divisible-by-three/1262-greatest-sum-divisible-by-three.cpp
@@ -1,28 +1,33 @@
 class Solution {
 public:
     int maxSumDivThree(vector<int>& nums) {
-        dp.resize(nums.size()  + 1 , vector<int> (4 ,-1 ));
-     int x =  func(nums , 0 , 3) ;
-    if(x < 0 ){
-        return 0 ; 
-    }
-   return x ; 
-    }
-private : 
-    vector<vector<int>> dp ; 
-    int func(vector<int> & v , int i ,int mod ){
-        if(i>= v.size() ){
-            if(mod == 0 ){
-            return 0 ; }
-            return INT_MIN ; 
+        dp.resize(nums.size()+ 1 , vector<int>( 4, -1)) ;  
+                  
+        int res =  func(nums , 0 , 0 ); 
+        if(res < 0 ){
+            return 0 ; 
         }
+        return res ; 
+      
         
-        if(dp[i][mod] != -1){
-            return dp[i][mod]; 
+    }
+private: 
+    vector<vector<int>> dp; 
+    int func(vector<int> & v  , int i , int mod ){
+        if(i == v.size()){
+            if(mod == 0 ){
+                return 0 ; 
+            }
+            return INT_MIN  ; 
         }
-        int x = v[i]%3; 
-        x = (x  + mod)%3; 
-        return dp[i][mod] = max(func(v , i + 1 , mod ) ,v[i] +  func(v , i +1 , x)); 
+        if(dp[i][mod]!= -1){
+            return dp[i][mod ]; 
+        }
+        int x  = v[i]% 3; 
+        x = (x + mod)%3; 
+        return dp[i][mod] =  max(func(v , i + 1 , mod) , v[i] + func(v , i + 1 , x ) );  
+        
+        
         
     }
 };
