b8f2a5674d7f31608cfbaeccc14cc2848b7d7cdd
Taran Tuteja <taran.tuteja@yahoo.com>
2022-10-28 12:21:00 +0530

    Time: 33 ms (78.31%), Space: 14.7 MB (45.97%) - LeetHub

--- 1326-minimum-number-of-taps-to-open-to-water-a-garden/1326-minimum-number-of-taps-to-open-to-water-a-garden.cpp
+++ 1326-minimum-number-of-taps-to-open-to-water-a-garden/1326-minimum-number-of-taps-to-open-to-water-a-garden.cpp
@@ -1,30 +1,17 @@
+
 class Solution {
-    // This Question is difficult to understand 
-    // This Question is important 
 public:
+    
     int minTaps(int n, vector<int>& A) {
-        vector<int>  dp (n + 1 , n +2); 
+     vector<int> dp(n + 1 , n+ 2);  
         dp[0] = 0 ; 
         for(int i = 0 ; i<= n ; i++){
-            int left = i - A[i]; 
-            int right = i + A[i] ; 
-            for(int j = max(0 , left ) ; j  <= min(i + A[i ] , n ) ; j++){
-                dp[j] = min(dp[j] , dp[max(0 , left )] + 1  ); 
+            int left = max(0  , i - A[i]); 
+            int right = min(i + A[i] , n); 
+            for(int j = left ; j <= right ; j ++){
+                dp[j] = min(dp[j] , dp[left] + 1 ); 
             }
         }
-        // for(auto x  : dp){
-        //     cout<< x <<" "; 
-        // }
-        return dp[n] < n +2 ?  dp[n] : -1 ; 
+        return dp[n] < n +2 ? dp[n] : -1; 
     }
 };
-
-
-
-         /*** 0      1      2      3      4      5
-          3      4      1      1      0      0    
-          ..........i1..........
-                  ....................i2......
-                 .........i3....
-                         ..........i4...
-           ....................i5..............***/    
